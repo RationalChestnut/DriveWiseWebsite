5b407c90571535d4ff12ec479181504628eddb56
Rational Chestnut <brian.kw.mason@gmail.com>
2024-09-21 22:59:23 -0400

    Deleted folders

--- src/MainNavigator.js
+++ src/MainNavigator.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Landing } from "./Landing/Landing.page";
 import styles from "./MainNavigator.module.css";
 import { Features } from "./Features/Features.page";
-import { Nav } from "./Nav/Nav.page";
 
 export const MainNavigator = () => {
   return (
     <div className={styles.firstContainer}>
-      {/* <Nav /> */}
       <div className={styles.container}>
         <div className={styles.contentContainer}>
           <Landing />
